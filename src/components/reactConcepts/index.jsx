e514746909f23a7dbfd3dd4e3114893439a3eafe
Olga Yudkin <cvtqx@protonmail.com>
2024-12-30 14:44:52 -0500

    updaetd readme

--- src/components/reactConcepts/index.jsx
+++ src/components/reactConcepts/index.jsx
@@ -1,39 +1,12 @@
-import { useState } from "react";
+import React, { useState } from 'react'
 
-//  const ParentComponent = () => {
-//   const [count, setCount] = useState(0);
-// console.log('parent rerendered')
-//   return (
-//     <div>
-//       <button onClick={() => setCount(count + 1)}>Increment</button>
-//       <ChildComponent count={count} />
-//     </div>
-//   );
-// };
-
-// const ChildComponent = ({ count }) => {
-//     console.log("ChildComponent rerendered");
-//   return <div>Count is {count}</div>;
-// };
-
-const ParentComponent = () => {
-  console.log("parent rerendered");
-  return (
-    <div>
-      <ChildComponent />
-    </div>
-  );
-};
-
-const ChildComponent = () => {
+export default function Counter() {
   const [count, setCount] = useState(0);
-  console.log("ChildComponent rerendered");
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>
-      <div>Count is {count}</div>
+      <h1>{count}</h1>
+      <button onClick = {() => setCount(count+1)}>Increment</button>
     </div>
-  );
-};
-
-export default ParentComponent;
+  )
+}
